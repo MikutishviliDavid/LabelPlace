7b4c7ebb8f2eb3a582b5102edede27df89476bca
DavidMikutishvili <davidstandartx@gmail.com>
2022-09-08 19:01:27 +0300

    Changed Dtos

--- LabelPlace.BusinessLogic/Dto/ProjectDto.cs
+++ LabelPlace.BusinessLogic/Dto/ProjectDto.cs
@@ -5,19 +5,23 @@ namespace LabelPlace.BusinessLogic.Dto
 {
     public class ProjectDto : BaseEntityDto
     {
+        public int UserId { get; set; }
+
+        public int CompanyId { get; set; }
+
         public string Title { get; set; }
 
         public string Description { get; set; }
 
         public string SourceDataUrl { get; set; }
 
-        public ProjectStatus Status { get; set; }
+        public ProjectStatus? Status { get; set; }
 
-        public ProjectType Type { get; set; }
+        public ProjectType? Type { get; set; }
 
         public UserDto User { get; set; }
 
-        public CompanyDto Company { get; set; } // int CompanyId - ?
+        public CompanyDto Company { get; set; }
 
         public HashSet<TextAnnotationDto> TextAnnotations { get; set; } = new HashSet<TextAnnotationDto>();
     }
