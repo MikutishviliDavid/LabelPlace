6ca2cdc80a3172a6ac749df98814a02078471aed
DavidMikutishvili <davidstandartx@gmail.com>
2022-09-16 19:11:17 +0300

    Added services

---
+++ LabelPlace.BusinessLogic/Services/CompanyService.cs
@@ -0,0 +1,30 @@
+﻿using LabelPlace.BusinessLogic.Dto;
+using LabelPlace.BusinessLogic.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LabelPlace.BusinessLogic.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        public Task<IEnumerable<CompanyDto>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task InsertAsync(CompanyDto company)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(CompanyDto company)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(CompanyDto company)
+        {
+            throw new System.NotImplementedException();
+        }  
+    }
+}
