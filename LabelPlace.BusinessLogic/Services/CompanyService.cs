52246679bc17c5b007a5610491b2b2c9541df745
DavidMikutishvili <davidstandartx@gmail.com>
2022-09-19 15:08:46 +0300

    Created CompanyController and edited CompanyService

--- LabelPlace.BusinessLogic/Services/CompanyService.cs
+++ LabelPlace.BusinessLogic/Services/CompanyService.cs
@@ -7,24 +7,6 @@ namespace LabelPlace.BusinessLogic.Services
 {
     public class CompanyService : ICompanyService
     {
-        public static List<CompanyDto> CompanyModels = new List<CompanyDto>
-        {
-            new CompanyDto
-            {
-                Id = 1,   
-                Name = "Apple",
-                Country = "USA",
-                City = "California"
-            },
-            new CompanyDto
-            {
-                Id = 2,
-                Name = "Microsoft",
-                Country = "USA",
-                City = "New Youk City"
-            }
-        };
-
         public async Task<IEnumerable<CompanyDto>> GetAllAsync()
         {
             throw new System.NotImplementedException();
@@ -52,7 +34,7 @@ namespace LabelPlace.BusinessLogic.Services
 
         public CompanyDto Find(int id)
         {
-            return CompanyModels.Find(c => c.Id == id);
+            throw new System.NotImplementedException();
         }
     }
 }
